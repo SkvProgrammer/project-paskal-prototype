352284110a6d78a8edb57891aaeba7554b5686bc
Satyam Kumar Verman <satyamvarman2004@gmail.com>
2024-12-12 11:53:57 +0530

    Update Comingsoon.js

--- src/components/Comingsoon.js
+++ src/components/Comingsoon.js
@@ -6,7 +6,7 @@ const Comingsoon = () => {
     <div className="coming-soon-container vh-100 d-flex align-items-center justify-content-center">
       <div className="text-center text-white coming-soon-content">
         <h1 className="display-3 fw-bold fade-in">We're Coming Soon!</h1>
-        <p className="lead fade-in delay-1">Our web app is under construction. Stay tuned!</p>
+        <p className="lead fade-in delay-1">Under construction 🤖. Stay tuned!</p>
         <div className="bubble-animation"></div>
       </div>
     </div>
